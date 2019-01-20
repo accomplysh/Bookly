422d4400c404f47ff240c68b29fee13bf44f8f1d
Albert Pinhasov <albert@accomplysh.com>
2019-01-20 18:07:04 -0500

    added Author property to Book model

--- Bookly/Models/Book.cs
+++ Bookly/Models/Book.cs
@@ -14,6 +14,8 @@ namespace Bookly.Models
         [StringLength(255)]
         public string Name { get; set; }
         public int YearPublished { get; set; }
+
+        public string Author { get; set; }
        // public Customer Customer { get; set; }
 
 
