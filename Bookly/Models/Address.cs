e4a45e380269ba71f2caebf01c802b498f21cb97
Albert Pinhasov <albert@accomplysh.com>
2019-01-20 18:16:27 -0500

    added Address Properties to Address Model

--- Bookly/Models/Address.cs
+++ Bookly/Models/Address.cs
@@ -7,5 +7,10 @@ namespace Bookly.Models
 {
     public class Address
     {
+        public string AddressName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string ZipCode { get; set; }
     }
 }
